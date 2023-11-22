5e223f5748fc18816343ec3a5d46314cbb9699f9
UshanKaushalya <Ushanwk22@gmail.com>
2023-11-22 18:05:59 +0530

    Some bug fixed

--- assest/js/Header.js
+++ assest/js/Header.js
@@ -3,9 +3,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener("scroll", function () {
     if (window.scrollY > 100) {
-    header.classList.add("scrolled");
-} else {
-    header.classList.remove("scrolled");
-}
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+    });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop - document.querySelector('header').offsetHeight,
+                behavior: "smooth"
+            });
+        });
+    });
 });
